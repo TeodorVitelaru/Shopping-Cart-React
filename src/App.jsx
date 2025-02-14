d3d20cb4699c0436dd280ccc298a131b3305cd4a
unknown <teodor_vitelaru@yahoo.com>
2025-02-14 11:07:57 +0200

    trying to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,17 @@ import Navbar from './components/navbar'
 function App() {
 
   return (
-    <Router>
+    <Fragment>
       <Navbar />
     <Routes>
+      <Route path='/' element={<ProductListPage/>} />
       <Route path='/products' element={<ProductListPage/>} />
       <Route path='/product-details/:id' element={<ProductDetailsPage/>} />
       <Route path='/cart' element={<CartListPage/>}/>
 
     </Routes>
     
-    </Router>
+    </Fragment>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import ShoppingCartProvider from './context/index.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Shopping-Cart-React">
+  <HashRouter >
     <ShoppingCartProvider>
       <App/>
     </ShoppingCartProvider>
     
-  </BrowserRouter>
+  </HashRouter>
 )
